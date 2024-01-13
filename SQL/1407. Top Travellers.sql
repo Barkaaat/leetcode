e067018aed8c99dd26e7dec761027b425c19f23b
Ahmed Barakat <ahmed0barakat001@gmail.com>
2024-01-13 23:56:27 +0200

    3

--- SQL/1407. Top Travellers.sql
+++ SQL/1407. Top Travellers.sql
@@ -1,13 +1,5 @@
-select
-	U.name,
-	isnull(sum(R.distance), 0) as travelled_distance
-from
-	Users U left join Rides R
-on
-	R.user_id = U.id
-group by
-	U.id,
-	U.name
-order by 
-	sum(R.distance) desc,
-	U.name asc;
+select U.name, isnull(sum(R.distance), 0) as travelled_distance
+from Users U left join Rides R
+on R.user_id = U.id
+group by U.id, U.name
+order by sum(R.distance) desc, U.name asc;
--- SQL/1587. Bank Account Summary II.sql
+++ SQL/1587. Bank Account Summary II.sql
@@ -1,11 +1,5 @@
-select
-	U.name,
-	sum(T.amount) as balance
-from
-	Users U left join Transactions T
-on
-	U.account = T.account
-group by
-	U.name
-having
-	sum(T.amount) > 10000
+select U.name, sum(T.amount) as balance
+from Users U left join Transactions T
+on U.account = T.account
+group by U.name
+having sum(T.amount) > 10000
